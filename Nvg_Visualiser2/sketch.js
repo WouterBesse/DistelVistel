21ae1170f4924d728bcf4c0f986a250ecfb73277
Wouter Besse <lolmaker5@gmail.com>
2022-08-18 19:28:01 +0200

    Made instancing work!

--- Nvg_Visualiser2/sketch.js
+++ Nvg_Visualiser2/sketch.js
@@ -1,35 +1,38 @@
 // Global Vars
 const VERBOSE = false;
-let scene;
-let scenechange = 0;
+const FRAMERATE = 60;
+let scene = 3;
+let sceneChange = false;
 let createdCanvas = 0;
-let lightMode; // Bepaalt of de achtergrond wit of zwart is
+let lightMode = 0; // Bepaalt of de achtergrond wit of zwart is
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
 
-// Functie om asset bestanden van tevoren in te laden
+let sceneDoms;
+let scenes;
+let sceneAllocations = [0, 1, 2, 2, 2];
+
+// Global Waves Vars
+let wColorMode = 0; // 0 = kleur, 1 = monochroom
+let wBlurWhiteMode;
+let wBlurDarkMode;
+let wFrequency;
+let wMovementCounter = 1;
+let wFrequencyAmplitude = [1, 1, 1];
+let wDistortionAmount;
+let wDistortionType = 0;
+
 function preload() {
-  fontRegular = loadFont('DDCHardware-Regular.ttf'); 
-  distelModel = loadModel('assets/Distel2.obj', true);
-  buurModel = loadModel('assets/BUUR.obj', true);
-  koshModel = loadModel('assets/KOSH.obj', true);
-  ureModel = loadModel('assets/URE.obj', true);
-  //distelRondModel = loadModel('assets/Distel.obj', true); // <------------------------ Breaks it all :(
+  scenes = [ // set Scenes
+    new p5(waveSketch),
+    new p5(distelSketch),
+    new p5(djSketch)
+  ];
+  sceneDoms = document.getElementsByClassName("p5Canvas");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  // Preset voor de kleuren van de lijnen bij kleur mode en monochroom mode
-  wColors = [
-    [color(255, 0, 0),
-    color(0, 255, 0),
-    color(0, 0, 255)]
-    ,
-    [color(255, 255, 255),
-    color(255, 255, 255),
-    color(255, 255, 255)]
-  ]
-
-  smooth(); // Anti aliasing
+  frameRate(FRAMERATE);
 
   //maak een connect-object aan dat zorgt voor de communicatie met oscServer.js
   connect = new Connect();
@@ -50,7 +53,9 @@ function setup() {
 }
 
 function waveVerbose() {
-  if(VERBOSE){
+  const SCENEID = 0;
+
+  if (VERBOSE) {
     console.group("Wave Variables");
     console.debug("wColorMode: ", wColorMode);
     console.debug("lightMode: ", lightMode);
@@ -66,9 +71,9 @@ function waveVerbose() {
 }
 
 // Ontvang de osc messages
-function oscReceiver(address,msg) {
+function oscReceiver(address, msg) {
   //als de variabele address gelijk is aan /y wordt de code tussen de {} uitegevoerd
-  switch(address){
+  switch (address) {
     case "/wColorMode":
       wColorMode = msg;
       break;
@@ -104,237 +109,272 @@ function oscReceiver(address,msg) {
       break;
     case "/scene":
       scene = msg;
-      scenechange = 1;
+      sceneChange = true;
       break;
   }
 }
 
-// Waves Vars
-let wColors = [[],[],[]];
-let wColorMode = 0; // 0 = kleur, 1 = monochroom
-let wBlurWhiteMode;
-let wBlurDarkMode;
-let wFrequency;
-let wPhase = 1;
-let wMovementCounter = 1;
-let wFrequencyAmplitude = [1,1,1];
-let wDistortionAmount;
-let wDistortionType = 0;
+// All sketch instances
+let waveSketch = function (p) {
+  const LINECOLORS = [
+    [p.color(255, 0, 0),
+    p.color(0, 255, 0),
+    p.color(0, 0, 255)]
+    ,
+    [p.color(255, 255, 255),
+    p.color(255, 255, 255),
+    p.color(255, 255, 255)]
+  ]; // Set wave line colors
+  let phase = 1;
 
-// Creëert een nieuwe canvas voor de scène indien nodig
-function uniqueCanvasCreator(id, engine) {
-  if(createdCanvas != id){
-    createCanvas(windowWidth, windowHeight, engine);
-    createdCanvas = id;
-  }
-}
-
-// Functie voor het renderen van dewaves
-function waveRender() {
-  waveVerbose();
-  uniqueCanvasCreator(1, P2D);
-  blendMode(BLEND);
-
-  // Kiezen tussen witte of zwarte achtergrond
-  switch(lightMode){
-    case 0:
-      background(255, 255, 255, wBlurWhiteMode);
-      blendMode(EXCLUSION);
-      break;
-    case 1:
-      background(0, 0, 0, wBlurDarkMode);
-      blendMode(SCREEN);
-      break;
+  p.setup = function () {
+    let canvas = p.createCanvas(windowWidth, windowHeight, p.P2D);
+    canvas.position(0, 0);
+    p.smooth(); // Anti aliasing
+    p.frameRate(FRAMERATE);
   }
 
-  // Aantal properties voor de lines
-  noFill();
-  strokeWeight(20);
+  p.drawBackground = function () {
+    p.blendMode(p.BLEND);
 
-  // Dit is de code om de waves horizontaal te laten scrollen
-  // Dit gebeurt door een getal om hoog te tellen
-  // De hoeveelheid dat hij per frame omhoog is geteld wordt omgezet naar de hoeveelheid dat hij scrollt
-  let movementDelta = wMovementCounter - wPhase;
-  wPhase += movementDelta * 0.05;
+    // Kiezen tussen witte of zwarte achtergrond
+    switch (lightMode) {
+      case 0:
+        p.background(255, 255, 255, wBlurWhiteMode);
+        p.blendMode(p.EXCLUSION);
+        break;
+      case 1:
+        p.background(0, 0, 0, wBlurDarkMode);
+        p.blendMode(p.SCREEN);
+        break;
+    }
+  }
 
-  // Hier maakt hij 3 waves (i < 3; i++)
-  for(var i = 0; i < 3; i++) {
+  p.calcWave = function (amplitude, line, w) {
+    // De algemene formule voor de waves, in principe een sinusgolf waarvan de hoogte wordt gemoduleerd met een sinusgolf van lagere frequentie
+    var h = amplitude * p.sin(w * 0.03 + phase * 0.07 + line * p.TWO_PI / 3) * p.pow(p.abs(p.sin(w * wFrequency + phase * 0.02)), 5);
+
+    // Dit is een array van 3 "distortion" formules om de waves nog cooler en meer responsive te maken
+    var waveTypes = [p.height / 2 + p.sin(wDistortionAmount * w) * wDistortionAmount * 100, p.height / 2 + p.sin(w * 0.0015 * p.TWO_PI * wDistortionAmount) * ((500 * wDistortionAmount) - 500), p.height / 2 * wDistortionAmount];
+    h += waveTypes[wDistortionType];
+    p.curveVertex(w, h);
+  }
+
+  p.drawWave = function (line) {
     // Deze waves krijgen elk de amplitude van een ander frequentie gebied
-    let ampl = wFrequencyAmplitude[i];
+    let ampl = wFrequencyAmplitude[line];
     // Deze waves krijgen elk een eigen kleur uit de waveColors array
-    stroke(wColors[wColorMode][i]);
+    p.stroke(LINECOLORS[wColorMode][line]);
 
-    beginShape();
-
-    // Hier wordt horizontaal voor elke pixel berekend hoe hoog de wave daar moet zijn
-    for(var w = -20; w < width + 20; w += 5) {
-      // De algemene formule voor de waves, in principe een sinusgolf waarvan de hoogte wordt gemoduleerd met een sinusgolf van lagere frequentie
-      var h = ampl * sin(w * 0.03 + wPhase * 0.07 + i * TWO_PI / 3) * pow(abs(sin(w * wFrequency + wPhase * 0.02)), 5);
-
-      // Dit is een array van 3 "distortion" formules om de waves nog cooler en meer responsive te maken
-      var waveTypes = [height / 2 + sin(wDistortionAmount * w)*wDistortionAmount*100, height / 2 + sin(w * 0.0015 * TWO_PI * wDistortionAmount)*((500*wDistortionAmount)-500), height / 2 * wDistortionAmount];
-      h += waveTypes[wDistortionType];
-      curveVertex(w, h);
+    p.beginShape();
+    for (var w = -20; w < p.width + 20; w += 5) { // Hier wordt horizontaal voor elke pixel berekend hoe hoog de wave daar moet zijn
+      p.calcWave(ampl, line, w);
     }
-    endShape();
+    p.endShape();
+  }
+
+  p.drawLines = function (strokeWeight) {
+    // Aantal properties voor de lines
+    p.noFill();
+    p.strokeWeight(strokeWeight);
+
+    // Dit is de code om de waves horizontaal te laten scrollen
+    // Dit gebeurt door een getal om hoog te tellen
+    // De hoeveelheid dat hij per frame omhoog is geteld wordt omgezet naar de hoeveelheid dat hij scrollt
+    let movementDelta = wMovementCounter - phase;
+    phase += movementDelta * 0.05;
+
+    // Hier maakt hij 3 waves (i < 3; i++)
+    for (var i = 0; i < 3; i++) {
+      p.drawWave(i);
+    }
+  }
+
+  p.draw = function () {
+
+    waveVerbose();
+
+    p.drawBackground();
+
+    p.drawLines(20);
   }
 }
 
-// Tekent de achtergrond
-function drawBackground(){
-  uniqueCanvasCreator(2, WEBGL);
-  blendMode(ADD);
+let distelSketch = function (p) {
+  const SCENEID = 1;
+  const DISTELXINSTANCES = 4;
+  const DISTELYINSTANCES = 2;
+  const TEXTCOLORS = [
+    p.color(0, 0, 0),
+    p.color(255, 255, 255)
+  ];
+  const BGCOLORS = [
+    p.color(255, 255, 255),
+    p.color(0, 0, 0)
+  ];
+  const STROKECOLORS = [
+    p.color(0, 0, 0),
+    p.color(255, 255, 255)
+  ];
+  let power = -DISTELYINSTANCES + 4;
 
-  // Kiezen tussen witte of zwarte achtergrond
-  switch(lightMode){
-    case 0:
-      background(255, 255, 255, 255);
-      stroke(0, 0, 0);
-      break;
-    case 1:
-      background(0, 0, 0, 255);
-      stroke(255, 255, 255);
-      break;
+  p.preload = function () {
+    fontRegular = p.loadFont('DDCHardware-Regular.ttf');
+    distelModel = p.loadModel('assets/Distel2.obj', true);
+    distelRondModel = p.loadModel('assets/Distel.obj', true);
   }
-}
 
-function distelRender(distel, DISTELXINSTANCES, DISTELYINSTANCES){
-  // Distel properties 
-  angleMode(DEGREES); 
-  strokeWeight(2);
-  rotateY(180);
-  scale(0.55); 
-  emissiveMaterial(255,0,146);
-  let power = -DISTELYINSTANCES+4;
-  translate((width/DISTELXINSTANCES)*(DISTELXINSTANCES/2+0.5), height/(Math.pow(DISTELYINSTANCES, power)), -300); // <------------ Moet minder beunoplossing worden
-  
-  for(var i = 0; i < DISTELXINSTANCES; i += 1){
-    translate((-width/DISTELXINSTANCES), -height);
-    for(var j = 0; j < DISTELYINSTANCES; j += 1){ 
-      translate(0, height/(DISTELYINSTANCES));
-      push();
-      switch(distel){
-        case distelModel:
-          scale((wFrequencyAmplitude[0]/192)*2/DISTELYINSTANCES);
-          break;
-        case distelRondModel:
-          rotateZ(millis() / 15);
-          scale(4);
-          break;
+  p.setup = function () {
+    let canvas = p.createCanvas(windowWidth, windowHeight, p.WEBGL);
+    canvas.position(0, 0);
+    p.smooth(); // Anti aliasing
+    p.frameRate(FRAMERATE);
+  }
+
+  p.drawBackground = function () {
+    p.blendMode(p.ADD);
+    p.background(BGCOLORS[lightMode]);
+    p.stroke(STROKECOLORS[lightMode]);
+  }
+
+  p.textRender = function (tekst) {
+    p.textFont(fontRegular);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textSize(p.width / 4);
+    p.fill(TEXTCOLORS[lightMode])
+    p.text(tekst, 0, 0);
+  }
+
+  p.drawDistel = function (distel) {
+    p.push();
+    switch (distel) {
+      case distelModel:
+        p.scale((wFrequencyAmplitude[0] / 192) * 2 / DISTELYINSTANCES);
+        break;
+      case distelRondModel:
+        p.rotateZ(millis() / 15);
+        p.scale(4);
+        break;
+    }
+    p.model(distel);
+    p.pop();
+  }
+
+  p.drawDistels = function () {
+    for (var i = 0; i < DISTELXINSTANCES; i += 1) {
+      p.translate((-p.width / DISTELXINSTANCES), -p.height);
+      for (var j = 0; j < DISTELYINSTANCES; j += 1) {
+        p.translate(0, p.height / (DISTELYINSTANCES));
+        p.drawDistel(distelModel);
       }
-      model(distel);
-      pop();
+    }
+  }
+
+  p.draw = function () {
+    p.drawBackground();
+    p.textRender("Distel");
+
+    // Distel properties 
+    p.angleMode(p.DEGREES);
+    p.strokeWeight(2);
+    p.rotateY(180);
+    p.scale(0.55);
+    p.emissiveMaterial(255, 0, 146);
+    p.translate((p.width / DISTELXINSTANCES) * (DISTELXINSTANCES / 2 + 0.5), p.height / (Math.pow(DISTELYINSTANCES, power)), -300); // <------------ Moet minder beunoplossing worden
+    p.drawDistels();
+
+  }
+}
+
+let djSketch = function (p) {
+  const BLINKTHRESH = 2;
+  const AMPTHRESH = 208;
+  const BGCOLORS = [
+    p.color(255, 255, 255),
+    p.color(0, 0, 0)
+  ];
+  const STROKECOLORS = [
+    p.color(0, 0, 0),
+    p.color(255, 255, 255)
+  ];
+  let canvas;
+
+  p.preload = function () {
+    buurModel = p.loadModel('assets/BUUR.obj', true);
+    koshModel = p.loadModel('assets/KOSH.obj', true);
+    ureModel = p.loadModel('assets/URE.obj', true);
+  }
+
+  p.setup = function () {
+    canvas = p.createCanvas(windowWidth, windowHeight, p.WEBGL);
+    canvas.position(0, 0);
+    p.smooth(); // Anti aliasing
+    p.frameRate(FRAMERATE);
+  }
+
+  p.playstate = function () {
+    canvas.hide();
+    p.noLoop();
+  }
+
+  p.drawBackground = function () {
+    p.blendMode(p.ADD);
+    p.background(BGCOLORS[lightMode]);
+    p.stroke(STROKECOLORS[lightMode]);
+  }
+
+  p.chooseDJ = function () {
+    switch (scene) {
+      case 2: // BUUR
+        return(buurModel);
+
+      case 3: // KOSH
+        return(koshModel);
+
+      case 4: // U:RE
+        return(ureModel);
+    }
+  }
+
+  p.draw = function () {
+    let dj = p.chooseDJ();
+
+    let blink = 0; //<--------------------------------- Kan miss beter in max worden gedaan // Ja, maar ik laat het voor nu ff in javascript
+    if (wFrequencyAmplitude[0] / AMPTHRESH > BLINKTHRESH) {
+      blink = 0;
+    } else {
+      blink = 1;
+    }
+
+    p.drawBackground();
+
+    let constrained = p.constrain(wFrequencyAmplitude[0] / AMPTHRESH, 1, 2);
+    p.ortho();
+    p.angleMode(p.DEGREES);
+    p.strokeWeight(2);
+    p.rotateX(180);
+    p.scale(4);
+    p.scale(constrained, 1);
+    p.emissiveMaterial(255, 0, 146);
+    p.model(dj);
+  }
+}
+
+function setScene(newScene) {
+  for(s = 0; s < scenes.length; s++) {
+    switch (s) {
+      case newScene:
+        scenes[s].loop();
+        break;
+      default:
+        scenes[s].noLoop();
+        scenes[s].clear();
     }
   }
 }
 
-//Render een aangegeven tekst op het scherm
-function textRender(tekst){
-  textFont(fontRegular);
-  textAlign(CENTER,CENTER);
-  textSize(width/4);
-  switch(lightMode){
-    case 0:
-      fill(0, 0, 0);
-      break;
-    case 1:
-      fill(255, 255, 255);
-      break;
-
-  }
-  text(tekst, 0, 0);
-}
-
-//Render de naam van de aangegeven DJ op het scherm
-function djRender(dj){
-  const blinkThresh = 2;
-  const ampThresh = 208;
-  uniqueCanvasCreator(2, WEBGL);
-  blendMode(ADD);
-
-  let blink = 0; //<--------------------------------- Kan miss beter in max worden gedaan
-  if(wFrequencyAmplitude[0]/ampThresh > blinkThresh){
-    blink = 0;
-  } else {
-    blink = 1;
-  }
-
-  switch(blink){
-    case 0:
-      background(255, 255, 255, 255);
-      stroke(0, 0, 0);
-      break;
-    case 1:
-      background(0, 0, 0, 255);
-      stroke(255, 255, 255);
-      break;
-  }
-
-  let constrained = constrain(wFrequencyAmplitude[0]/ampThresh, 1 , 2);
-  ortho();
-  angleMode(DEGREES); 
-  strokeWeight(2);
-  rotateX(180);
-  scale(4);
-  scale(constrained, 1);
-  emissiveMaterial(255,0,146);
-  model(dj);
-}
-
-// De functie waarin de frame getekend wordt
 function draw() {
-  switch(scene) {
-    case 0: // Waves
-      if(scenechange){
-        uniqueCanvasCreator(1, P2D);
-        scenechange = 0;
-      }
-      waveRender();
-      break;
-
-    case 1: // Distel
-      if(scenechange){
-        uniqueCanvasCreator(2, WEBGL);
-        scenechange = 0;
-      }
-      drawBackground();
-      textRender("Distel");
-      distelRender(distelModel, 4, 2);
-      break;
-
-    case 2: // BUUR
-      if(scenechange){
-        uniqueCanvasCreator(2, WEBGL);
-        scenechange = 0;
-      }
-      djRender(buurModel);
-      break; 
-
-    case 3: // KOSH
-      if(scenechange){
-        uniqueCanvasCreator(2, WEBGL);
-        scenechange = 0;
-      }
-      djRender(koshModel);
-      break;
-
-    case 4: // U:RE
-      if(scenechange){
-        uniqueCanvasCreator(2, WEBGL);
-        scenechange = 0;
-      }
-      djRender(ureModel);
-      break; 
-
-    case 5: // Distel rond <-------------------- Problematisch idk wrm
-      if(scenechange){
-        uniqueCanvasCreator(2, WEBGL);
-        scenechange = 0;
-      }
-      drawBackground();
-      distelRender(distelRondModel, 1, 1);
-      break; 
-
+  if(sceneDoms.length >= 4) {
+    setScene(sceneAllocations[scene]);
   }
-
 }
